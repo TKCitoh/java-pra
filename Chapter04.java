03c219ef5f5cdb769bf95e84a7dae8b498b9e726
tkc <tkci10.pooh@gmail.com>
2023-03-07 18:40:20 +0900

    Java4章

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name1 = "takashi";
+			System.out.println(name1);
+			
+		name1 = "yarebadekiru";
+			System.out.println(name1);
+			
+		final int number1 = 7;
+			System.out.println(number1);
+	}
+}
